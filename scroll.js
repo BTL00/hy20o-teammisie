27a48816c8dbe66b5804f5a51cebdd0211d85862
Piotr Bomba <bombapiotr@gmail.com>
2020-04-04 17:05:08 +0200

    initial2

--- scroll.js
+++ scroll.js
@@ -1,19 +1,14 @@
-let max_x = window.width;
-let max_y = window.height;
+var height = document.documentElement.scrollHeight;
+var width = document.documentElement.scrollHeight;
 
+function scroll(event){
+    var y = event.clientY;
+    var x = event.clientX;
 
+    var yPercentage = y/screen.height;
+    var xPercentage = x/screen.width;
 
-let y = window.scrollY;
-let x = window.scrollX;
+    window.scrollTo(2*xPercentage*width,3*yPercentage*height);
+}
 
-let canv = document.querySelector("div#strona");
-
-canv.addEventListener('mousemove', e => {
-  console.log('evnet');
-  console.log(canv.offsetWidth);
-	newpos_x = canv.offsetWidth * e.pageX / max_x; 
-  newpos_y = canv.offsetHeight * e.pageY / max_y; 
-
-	window.scrollTo(newpos_x, newpos_y);
-  
-});
+window.onmousemove = scroll;
